b34e20d2dd307e303b43d4d13ec0655c0b625f48
Øyvind Kallstad <kallstad@gmail.com>
2018-01-21 14:21:45 +0100

    update install script

--- arch/install-vars.sh
+++ arch/install-vars.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 KEYBOARD_LAYOUT="no-latin1"
+WIFI_SETUP="true"
+WIFI_INTERFACE="wlo1"
+WIFI_SSID="fjellheim5-1"
--- arch/install.sh
+++ arch/install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# wget
+
 source ./install-vars.sh
 
+if [ "${WIFI_SETUP}" -eq "true" ]; then
+    echo "Please enter password for wireless network '${WIFI_SSID}':"
+    read -s WIFI_PASSWORD
+fi
+
 loadkeys ${KEYBOARD_LAYOUT}
+
+if [ -d "/sys/firmware/efi/efivars" ]; then
+    BOOT_MODE="UEFI"
+else
+    BOOT_MODE="BIOS"
+fi
+
+systemctl start dhcpcd
+
+ln -s /usr/share/dhcpcd/hooks/10-wpa_supplicant /usr/lib/dhcpcd/dhcpcd-hooks/
+
+cat <<EOT >> /etc/wpa_supplicant/wpa_supplicant-${WIFI_INTERFACE}.conf
+network={
+    ssid="${WIFI_SSID}"
+    psk="${WIFI_PASSWORD}"
+}
+EOT
